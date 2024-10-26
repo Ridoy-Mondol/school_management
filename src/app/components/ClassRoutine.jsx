04b217a095d47b7b81bc498f28f2e076f4f2d0c0
Ridoy Mondol <ridoymondol140@gmail.com>
2024-10-27 00:57:51 +0600

    fixed small design issue

--- src/app/components/ClassRoutine.jsx
+++ src/app/components/ClassRoutine.jsx
@@ -69,13 +69,13 @@ const ClassRoutine = () => {
   <div className=" w-full p-2 shadow-[inset_10px_10px_4px_20px_#00000040] pb-[15px] pr-[15px] my-[127px] bg-white overflow-hidden flex flex-col">
     {Data.map((item) => (
       <div className="flex items-center justify-between" key={item.id}>
-        <p className={`w-[13.75%] h-[127px] flex items-center justify-center mt-[10px] text-white font-semibold font-openSans text-[18px] leading-[24.51px] bg-darkRed ${item.id === 1 ? 'bg-[#14238A]' : ''}`}>
+        <p className={`w-[13.75%] h-[127px] flex items-center justify-center mt-[10px] text-white font-semibold font-openSans text-[18px] leading-[24.51px] ${item.id === 1 ? 'bg-[#14238A]' : 'bg-darkRed'}`}>
           {item.day}
         </p>
         {[item.period1, item.period2, item.period3, item.period4, item.period5, item.period6].map((period, index) => (
           <p
             key={index}
-            className={`w-[13.75%] h-[127px] flex items-center justify-center mt-[10px] text-dark font-medium font-poppins text-[16px] leading-[24px] bg-[#CCCCCC] ${item.id === 1 ? 'bg-[#14238A] text-white font-semibold text-[18px] leading-[27px]' : ''}`}
+            className={`w-[13.75%] h-[127px] flex items-center justify-center mt-[10px] text-dark font-medium font-poppins text-[16px] leading-[24px] ${item.id === 1 ? 'bg-[#14238A] text-white font-semibold text-[18px] leading-[27px]' : 'bg-[#CCCCCC]'}`}
           >
             {period}
           </p>
